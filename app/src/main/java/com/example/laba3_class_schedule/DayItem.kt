625db3377107570185cf1f04b22dc83387e7534e
Dmitry <miller20.03@mail.ru>
2024-01-13 07:10:40 +0500

    all_additions

---
+++ app/src/main/java/com/example/laba3_class_schedule/DayItem.kt
@@ -0,0 +1,6 @@
+package com.example.laba3_class_schedule
+
+data class DayItem(
+    val day: String,
+    val lessons: List<SubjectItem>
+)
---
+++ app/src/main/java/com/example/laba3_class_schedule/DayViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.laba3_class_schedule
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class DayViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    val lessonsRecyclerView: RecyclerView = view.findViewById(R.id.list_subject)
+}
